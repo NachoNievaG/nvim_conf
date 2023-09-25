e0e1ec5d8e8ba2148133c35ae2f192230b3e28d1
NachoNievaG <jose.ign.nieva@gmail.com>
2023-09-24 23:41:25 -0300

    fix(lsp-zero): extend_lspconfig

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -97,9 +97,8 @@ return {
 
       local lsp_zero = require('lsp-zero')
 
+      lsp_zero.extend_lspconfig()
       lsp_zero.on_attach(function(client, bufnr)
-        -- see :help lsp-zero-keybindings
-        -- to learn the available actions
         lsp_zero.default_keymaps({ buffer = bufnr })
       end)
 
